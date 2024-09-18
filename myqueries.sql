e6a444e585202792324df6ced88f65124a967446
Elissa Borges <141645429+redroselilith@users.noreply.github.com>
2024-09-18 08:49:17 -0300

    Add files via upload

--- myqueries.sql
+++ myqueries.sql
@@ -17,5 +17,28 @@ group by d.name;
 select d.name, min(e.start_date), CONCAT(e.fname, ' ', e.lname)name
 from department d, employee e
 where e.dept_id = d.dept_id
-group by d.name;
+group by d.name, e.employee;
 
+select CONCAT(fname, ' ', lname)name, e.dept_id, start_date
+from employee e, (select min(start_date)posse, dept_id
+from employee
+group by dept_id)datas
+where start_date = datas.posse and e.dept_id = datas.dept_id;
+
+with cte as (
+select min(start_date)posse, dept_id
+from employee
+group by dept_id)
+select fname, lname, posse, employee.dept_id
+from employee, cte
+where start_date = posse and employee.dept_id = cte.dept_id;
+
+create view per_dept as
+select min(start_date)posse, dept_id
+from employee
+group by dept_id;
+
+select e.fname, e.lname, per_dept.posse, e.dept_id
+from employee e, per_dept
+where e.start_date = per_dept.posse
+and e.dept_id = per_dept.dept_id;
